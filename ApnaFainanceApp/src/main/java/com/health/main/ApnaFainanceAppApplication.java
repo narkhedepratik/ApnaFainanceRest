d3143979062b23e456f4be528c571ed392e81d06
narkhedepratik <pratiknarkhede039@gmail.com>
2024-01-10 10:14:42 +0530

    Maven project created

---
+++ ApnaFainanceApp/src/main/java/com/health/main/ApnaFainanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.health.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApnaFainanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApnaFainanceAppApplication.class, args);
+	}
+
+}
---
+++ ApnaFainanceApp/src/test/java/com/health/main/ApnaFainanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApnaFainanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
