06c520cf8003d854f12e4ad91b86396375fb1be1
narkhedepratik <pratiknarkhede039@gmail.com>
2024-01-18 10:50:34 +0530

    Merge customer post api

--- ApnaFainanceApp/src/main/java/com/health/main/repository/CustomerRepository.java
+++ ApnaFainanceApp/src/main/java/com/health/main/repository/CustomerRepository.java
@@ -1,11 +1,13 @@
 package com.health.main.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.health.main.model.Customer;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+public interface CustomerRepository extends JpaRepository<Customer, UUID>{
 
 }
